45490b461bd319f58f7754cbd4072a8c1a2d8e08
JuliaSants <julia3053@gmail.com>
2020-07-04 21:06:52 -0300

    init

--- vue-app/src/components/index.vue
+++ vue-app/src/components/index.vue
@@ -1,85 +1,14 @@
 <template>
   <div class="hello">
+     <form method="get" action=".">
+        <input v-model="name" placeholder="name" name="name" type="text" id="name" value="" size="10" maxlength="9" />
+        <input v-model="email" placeholder="email" name="email" type="email" id="email" size="60" />
+        <input v-model="telefone" placeholder="telefone" name="telefone" type="tel" id="telefone" size="40" />
+        <input v-model="cpf" placeholder="cpf" name="cpf" type="tel" id="cpf" size="40" />
+      </form>
+      {{'nome:' + name}}
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
   </div>
 </template>
 
@@ -88,10 +17,14 @@ export default {
   name: 'index',
   data () {
     return {
-      msg: 'Teste Full Stack Qualicor'
+      msg: 'Teste Full Stack Qualicor',
+      name: '',
+      email: '',
+      cpf: '',
+      telefone: ''
     }
   }
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
